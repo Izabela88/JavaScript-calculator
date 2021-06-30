e8ed79b0fa62339dee3058865ea56d92b50676f0
izak <izabkru88@gmail.com>
2021-06-30 13:43:17 +0100

    split code into functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,17 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
   // parseInt() function convert string ito a number
-  currentResult = currentResult + parseInt(userInput.value);
+  currentResult = currentResult + enteredNumber;
   // or currentResult = currentResult + +userInput.value; /without decimal places
-  outputResult(currentResult, '');
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
