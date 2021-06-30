49ccd6488edbfbc4a98242853eb7fdd5a4092814
izak <izabkru88@gmail.com>
2021-06-30 13:30:29 +0100

    add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,11 @@
-alert('This works');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add() {
+  // parseInt() function convert string ito a number
+  currentResult = currentResult + parseInt(userInput.value);
+  // or currentResult = currentResult + +userInput.value; /without decimal places
+  outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click', add);
